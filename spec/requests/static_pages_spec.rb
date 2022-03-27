8f34ff85b32721425081db96b6182028cb82a83a
sako <naotolj.fox@gmail.com>
2022-03-27 22:38:27 +0900

    レスポンステスト実装

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "トップページ" do
+    it "正常にレスポンスを返すこと" do
+      get root_path
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "aboutページ" do
+    it "正常にレスポンスを返すこと" do
+      get about_path
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "利用規約ページ" do
+    it "正常にレスポンスを返すこと" do
+      get terms_path
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+  end
+end
