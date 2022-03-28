620e19daa61fcc499cd4b23fc441d966ac4d2e09
sako <naotolj.fox@gmail.com>
2022-03-28 22:38:47 +0900

    トップページ・aboutページ・規約ページの文字表示テスト実装

---
+++ spec/system/static_pages_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :system do
+  describe "トップページ" do
+    context "ページ全体" do
+      before do
+        visit root_path
+      end
+
+      it "エンカの文字が表示されていること" do
+        expect(page).to have_content 'エンカ'
+      end
+    end
+  end
+
+  describe "aboutページ" do
+    before do
+      visit about_path
+    end
+
+    it "エンカシェアとは？の文字が表示されていること" do
+      expect(page).to have_content 'エンカシェアとは？'
+    end
+  end
+
+  describe "利用規約ページ" do
+    before do
+      visit terms_path
+    end
+
+    it "利用規約の文字が表示されていること" do
+      expect(page).to have_content '利用規約'
+    end
+  end
+end
