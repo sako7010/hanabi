43e21fc4281f06d4b857c634231ab92cd6ae4d2c
sako <naotolj.fox@gmail.com>
2022-04-02 16:06:06 +0900

    deviceインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root               to: 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :terms,        to: 'static_pages#terms'
