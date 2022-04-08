bf7a1c6d5a4e7ef5d43e9480a609d93b11a15586
sako <naotolj.fox@gmail.com>
2022-04-08 20:41:33 +0900

    rubocop自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   before_save :downcase_email
   validates :user_name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-private
+  private
+
   def downcase_email
     self.email = email.downcase
   end
