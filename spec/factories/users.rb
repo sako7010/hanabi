28227c2c7cf772c22c0daf3236b11f69530d6d2e
sako <naotolj.fox@gmail.com>
2022-04-16 16:05:55 +0900

    FactoryBotダミー設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     user_name { Faker::Name.name }
     sequence(:email) { |n| "example#{n}@example.com" }
-    password { "foobar" }
-    password_confirmation { "foobar" }
+    password { "password" }
+    password_confirmation { "password" }
     profile { "はじめまして" }
   end
 end
