3146d3e0aaefeff37ffe6001c5e57cb9a9cc1014
sako <naotolj.fox@gmail.com>
2022-04-16 16:06:30 +0900

    ログイン、ログアウト、プロフィール画面テスト実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,5 +65,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include Devise::Test::IntegrationHelpers, type: :system
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include FactoryBot::Syntax::Methods
 end
