0bc8ae03d7e188d5cb71dd27806d5d66288dc80e
sako <naotolj.fox@gmail.com>
2022-04-21 21:17:14 +0900

    不要なため削除

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,3 @@
 module UsersHelper
-  def gravatar_for(user, size: 80)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.user_name, class: "gravatar")
-  end
+
 end
