14149433080f1114b0ba4816733cb3dfc2944a5e
sako <naotolj.fox@gmail.com>
2022-04-25 22:33:36 +0900

    関連付け

--- spec/models/hanabi_spec.rb
+++ spec/models/hanabi_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Hanabi, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:hanabi){ create(:hanabi) }
+
+  describe "hanabiモデル" do
+    context "バリデーション" do
+      xit "一旦放置" do
+        expect(page).to have_content 'きれいな花火　一緒に観よ'
+      end
+    end
+  end
 end
