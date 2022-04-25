3ffe81ce701409d0c0196677947883214ad0606e
sako <naotolj.fox@gmail.com>
2022-04-25 20:06:52 +0900

    hanabiモデルバリデーション設定とダミーデータの設定

--- spec/factories/hanabis.rb
+++ spec/factories/hanabis.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :hanabi do
     user_id { 1 }
-    title { "MyString" }
-    body { "MyText" }
+    title { "人混みを避けたスポットはここ" }
+    body { "ここからは小さいけど丸いのが見える！　近くにカフェあるよ！" }
     image_id { "MyString" }
+    address { "Faker::Address.street_address" }
   end
 end
