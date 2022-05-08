e21a7b1b29d3d31a0821e6192c2e83df749298ef
sako <naotolj.fox@gmail.com>
2022-05-08 09:51:28 +0900

    花火モデルバリデーションテスト

--- spec/models/hanabi_spec.rb
+++ spec/models/hanabi_spec.rb
@@ -5,8 +5,32 @@ RSpec.describe Hanabi, type: :model do
 
   describe "hanabiモデル" do
     context "バリデーション" do
-      xit "一旦放置" do
-        expect(page).to have_content 'きれいな花火　一緒に観よ'
+      it "有効な状態であること" do
+        expect(hanabi).to be_valid
+      end
+
+      it "タイトルがなければ無効な状態であること" do
+        hanabi = build(:hanabi, title: nil)
+        hanabi.valid?
+        expect(hanabi.errors[:title]).to include("を入力してください")
+      end
+
+      it "タイトルが30文字以内であること" do
+        hanabi = build(:hanabi, title: "あ" * 31)
+        hanabi.valid?
+        expect(hanabi.errors[:title]).to include("は30文字以内で入力してください")
+      end
+
+      it "住所が50文字以内であること" do
+        hanabi = build(:hanabi, address: "あ" * 51)
+        hanabi.valid?
+        expect(hanabi.errors[:address]).to include("は50文字以内で入力してください")
+      end
+
+      it "説明が140文字以内であること" do
+        hanabi = build(:hanabi, body: "あ" * 141)
+        hanabi.valid?
+        expect(hanabi.errors[:body]).to include("は140文字以内で入力してください")
       end
     end
   end
