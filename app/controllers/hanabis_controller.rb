495ac90c2bb29f4770aad041bae8edcfe6002664
sako <naotolj.fox@gmail.com>
2022-05-08 09:40:57 +0900

    花火モデル作成、ページレイアウト仮設定

--- app/controllers/hanabis_controller.rb
+++ app/controllers/hanabis_controller.rb
@@ -1,13 +1,50 @@
 class HanabisController < ApplicationController
+  before_action :user_signed_in?
+
   def index
+    @user = current_user
+    @hanabis = Hanabi.all
   end
 
   def show
+    @hanabi = Hanabi.find(params[:id])
   end
 
   def new
+    @user = current_user
+    @hanabi = Hanabi.new
+  end
+
+  def create
+    @hanabi = Hanabi.new(hanabi_params)
+    @hanabi.user_id = current_user.id
+    if @hanabi.save
+      flash[:notice] = "花火スポットを登録しました"
+      redirect_to hanabi_path(@hanabi)
+    else
+      render "new"
+    end
   end
 
   def edit
+    @hanabi = Hanabi.find(params[:id])
+  end
+
+  def update
+    @hanabi = Hanabi.find(params[:id])
+    @hanabi.update(hanabi_params)
+    redirect_to @hanabi
+  end
+
+  def destroy
+    @hanabi = Hanabi.find(params[:id])
+    @hanabi.destroy
+    flash[:success] = "削除しました"
+    redirect_to :hanabis
+  end
+
+  private
+  def hanabi_params
+    params.require(:hanabi).permit(:title, :body, :address, :image)
   end
 end
--- app/models/hanabi.rb
+++ app/models/hanabi.rb
@@ -1,6 +1,8 @@
 class Hanabi < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :name, presence: true, length: { maximum: 30 }
-  validates :body, length: { maximum: 140 }
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :address, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 140 }
+  attachment :image
 end
